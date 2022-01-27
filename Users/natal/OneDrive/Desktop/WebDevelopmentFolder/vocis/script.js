e6954e5879767ba3f5b2d974ac761cbc8d271366
Natalija <natalija.greiner@gmail.com>
2022-01-27 11:35:59 +0100

    First commit of a page under construction.

---
+++ Users/natal/OneDrive/Desktop/WebDevelopmentFolder/vocis/script.js
@@ -0,0 +1,36 @@
+var numberOfHovers = document.querySelectorAll(".hvrs").length;
+
+function makeSound(key) 
+{
+
+  switch (key) 
+  {
+    case "v":
+      var vocis = new Audio("audio/vocis.mp3");
+      vocis.play();
+      break;
+
+    case "s":
+      var simona = new Audio("audio/simona3.mp3");
+      simona.play();
+      break;
+
+    case "b":
+      var husband = new Audio("audio/bor2.mp3");
+      husband.play();
+      break;
+
+  }
+}
+
+document.addEventListener("keypress", function(event) 
+{
+  makeSound(event.key);
+});
+
+
+
+
+
+
+
